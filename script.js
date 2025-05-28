299df133579cc7a1ea0d974a30a38bb954c82eb2
UAndrew2 <andrewcar@ukr.net>
2025-05-28 13:21:53 +0300

    update

--- script.js
+++ script.js
@@ -80,3 +80,106 @@ mainUnits.forEach(mainUnit => {
         );
     }
 });
+
+/* function resizeIframe(iframe) {
+    iframe.style.height = iframe.contentWindow.document.documentElement.scrollHeight + 'px';
+}
+
+const labPages = Array.from($_lab_content.querySelectorAll('iframe.js-page-iframe'));
+
+labPages.forEach(function(iframe) {
+    iframe.onload = () => {resizeIframe(iframe)};
+}); */
+
+/* if ($_lab_menu && $_lab_content) {
+    const units = Array.from($_lab_content.querySelectorAll('.js-lab-unit'));
+
+    if (units.length > 0) {
+        const selectUnit = (unitToSelect) => {
+            units.forEach(unit => {
+                const isSelected = (unit === unitToSelect);
+                unit.style.display = isSelected ? '' : 'none';
+                if (unit.tabButton) {
+                    unit.tabButton.classList.toggle('selected', isSelected);
+                }
+            });
+        };
+
+        units.forEach(unit => {
+            const title = unit.dataset.title || 'Вкладинка';
+            const tabButton = $_dce('div');
+            
+            tabButton.classList = ['button border-radius js-main-menu-button'];
+            tabButton.textContent = title;
+            
+            tabButton.addEventListener('click', () => selectUnit(unit));
+            
+            unit.tabButton = tabButton; 
+            
+            $_lab_menu.append(tabButton);
+            
+            const submenu = $_dce('div');
+            submenu.classList = ['lab-unit-menu js-lab-unit-menu'];
+            
+            unit_items = Array.from(unit.querySelectorAll('.js-lab-unit-item'));
+            unit_items.forEach(u_item => {
+                const sm_title = unit.dataset.title || 'Пункт';
+                const submenu_item = $_dce('div');
+                submenu_item.innerText = sm_title;
+                submenu.append(submenu_item);
+            });
+            
+            unit.insertBefore(submenu, unit.firstChild);
+        });
+
+        selectUnit(units[0]);
+    }
+}
+ */
+
+
+/* $_lab_content.units = [];
+
+$_lab_content.selectTab = function (lab_unit) {
+    const selected = $_lab_content.units.filter(lu => lu === lab_unit);
+    if (selected.length < 1)
+        return;
+    
+    selected[0].switch_it(true);
+    $_lab_content.units.filter(lu => lu !== selected[0]).forEach(lu => lu.switch_it(false));
+}
+
+$_lab_menu.tabs = [];
+$_lab_menu.addTab = function(lab_unit) {
+    if ( this.tabs.includes(lab_unit) )
+        return;
+    const title = lab_unit.dataset.title;
+    const div = $_dce('div');
+    div.classList = ['js-main-menu-button'];
+    div.innerText = title;
+    div.addEventListener('click', () => $_lab_content.selectTab(lab_unit));
+    lab_unit.select_button = div;
+    this.append(div);
+}
+
+$_lab_content.querySelectorAll('.js-lab-unit').forEach(function(lab_unit) {
+    $_lab_menu.addTab(lab_unit);
+});
+
+$_lab_content.units = (function () {
+    const units = [...this.querySelectorAll('.js-lab-unit')];
+    units.forEach(u => {
+        u.switch_it = is_on => {
+            if (is_on) {
+                u.style.display = '';
+                if (u.select_button !== undefined)
+                    u.select_button.classList.add('selected');
+            } else {
+                u.style.display = 'none';
+                if (u.select_button !== undefined)
+                    u.select_button.classList.remove('selected');
+            }
+        }
+    });
+    return units;
+})(); */
